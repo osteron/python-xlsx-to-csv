a8b98285e574995ae878f2b5b8b1599d34a9fe0d
osteron <49269998+osteron@users.noreply.github.com>
2022-10-17 17:26:13 +0300

    Изменена логика

--- excel_parser.py
+++ excel_parser.py
@@ -1,4 +1,5 @@
 import csv
+import sys
 import time
 from progress.bar import IncrementalBar
 import win32com.client as win32
@@ -7,21 +8,24 @@ from pathlib import Path
 from win32com.client import constants as c
 
 
-def xls_to_csv(xls_file, csv_file):
+def xls_to_csv(xls_file_input, csv_file_output):
     dir_path = pathlib.Path.cwd()
-    file_name = Path(dir_path, xls_file)
+    file_name = Path(dir_path, xls_file_input)
     excel = win32.gencache.EnsureDispatch('Excel.Application')
+    excel.DisplayAlerts = False
     try:
         wb = excel.Workbooks.Open(file_name)
     except:
-        input(f"\n\tФайла {xls_file} не обнаружено. Нажмите Enter, чтобы выйти.")
-        exit(1)
-    result_path = str(dir_path) + "\\" + csv_file
+        print(f"\tФайл {xls_file_input} не обнаружен. Завершение работы через 3 секунды.\n")
+        time.sleep(3)
+        sys.exit()
+    result_path = str(dir_path) + "\\" + csv_file_output
     wb.SaveAs(result_path, c.xlCSV)
     wb.Close()
 
 
-def templates_search(job: str, dep: str, matrix: csv, template_set: set):
+def templates_search(job: str, dep: str, matrix, template_set: set):
+    xtemp = ''
     for z in range(1, len(matrix)):
         if job != "None" and str(matrix[z][2]).upper() in job.upper() and str(matrix[z][3]).upper() in dep.upper():
             xtemp = str(matrix[z][5]).upper()
@@ -32,7 +36,7 @@ def templates_search(job: str, dep: str, matrix: csv, template_set: set):
     return xtemp
 
 
-def set_and_result(set_template: set, template_mms: str):
+def set_and_result(set_template: set, template_app: str):
     set_template.discard('NONE')
     if '' in set_template and len(set_template) == 1:
         set_template.discard('')
@@ -42,14 +46,14 @@ def set_and_result(set_template: set, template_mms: str):
         set_template.discard('')
     if '##' and '##+##' in set_template:
         set_template.discard('##')
-    if '##' in set_template and '##_###' in set_template:
-        set_template.discard('##_###')
-    if '##_###' in set_template and len(set_template) > 1:
-        set_template.discard('##_###')
+    if '##' in set_template and '##_SSO' in set_template:
+        set_template.discard('##_SSO')
+    if '##_SSO' in set_template and len(set_template) > 1:
+        set_template.discard('##_SSO')
         set_template.add('##')
-    if '########_####_###' in set_template and len(set_template) > 1:
+    if '###########_SSO' in set_template and len(set_template) > 1:
         set_template.clear()
-        set_template.add('########_####_###')
+        set_template.add('###########_SSO')
     if '##' in set_template and len(set_template) > 1:
         set_template.clear()
         set_template.add('##')
@@ -58,100 +62,105 @@ def set_and_result(set_template: set, template_mms: str):
     if result_template == "":
         result_template = "ДОСТУП НЕ НАЙДЕН"
 
-    return result_template, "True" if result_template == template_mms else "False"
+    return result_template, "True" if result_template == template_app else "False"
+
+
+def with_open_file_to_list(file_to_list: csv):
+    try:
+        with open(file_to_list) as file_csv:
+            file_reader = csv.reader(file_csv, delimiter=";")
+            to_list = list(list(file_reader))
+        file_csv.close()
+        return to_list
+    except FileNotFoundError:
+        print(f"\tФайл {file_to_list} не обнаружен. Завершение работы через 3 секунды.")
+        time.sleep(3)
+        sys.exit()
 
 
 if __name__ == '__main__':
     try:
-        input("\n\tДобрый день!\n\n\tНеобходимо добавить файлы в папке со скриптом:\n\t- app_users.csv"
-              "\n\t- matrix.csv\n\t- job_combination.XLS\n\n\tИ нажать Enter.")
+        print("\n\tДобрый день!\n\n\tПроверяю наличие файлов:\n\t- app_users.csv"
+              "\n\t- matrix.csv\n\t- job_combination.XLS\n")
         start_time = time.time()
         xls_to_csv('job_combination.XLS', 'job_combination.csv')
-        
-        with open('job_combination.csv') as jobfile:
-            reader = csv.reader(jobfile, delimiter=';')
-            joblst = list(list(reader))
+        job_list = with_open_file_to_list('job_combination.csv')
+        matrix_list = with_open_file_to_list('matrix.csv')
+        app_list = with_open_file_to_list('app_users.csv')
 
-        with open('matrix.csv') as matrix:
-            reader = csv.reader(matrix, delimiter=';')
-            matrixlst = list(list(reader))
+        print("\tФайлы на месте, начинается обработка.\n")
 
-        with open('app_users.csv') as appfile:
-            reader = csv.reader(appfile, delimiter=';')
-            applst = list(list(reader))
-
-        print('\n\tНачинается выполнение...\n')
-        csv_file = open('###_parser_result.csv', 'w', newline='')
+        csv_file = open('app_parser_result.csv', 'w', newline='')  # путь к файлу csv
         with csv_file as file:
             writer = csv.writer(file, delimiter=';')
             writer.writerows(
                 [['APP_LOGIN;CLIENT;TEMPLATE;RESULT;IF-ELSE;TTH;LOCATION;JOB1;DEP1;JOB2;DEP2;JOB3;DEP3;JOB4;DEP4;JOB5;DEP5'
                   ';TEMP1;TEMP2;TEMP3;TEMP4;TEMP5']]
             )
-            bar = IncrementalBar('Progress', max=len(applst)-1)
+            bar = IncrementalBar('\tProgress', max=len(app_list) - 1)
 
-            for x in range(1, len(applst)):
+            for x in range(1, len(app_list)):
                 low = 1
-                high = len(joblst)
+                high = len(job_list)
                 while (high - low) > 1:
                     mid = (low + high) // 2
-                    if applst[x][6] > joblst[mid][0]:
+                    if app_list[x][6] > job_list[mid][0]:
                         low = mid
-                    elif applst[x][6] < joblst[mid][0]:
+                    elif app_list[x][6] < job_list[mid][0]:
                         high = mid
                     else:
                         break
                     mid = (low + high) // 2
-                if joblst[mid][4] in ['####', '####', '####', '###'] or joblst[mid][8] in \
+                if job_list[mid][4] in ['####', '####', '####', '###', '####'] or job_list[mid][8] in \
                         {
-                            '##########',
-                            '################',
-                            '#######',
-                            '####################################',
-                            '######################',
-                            '################',
+                            '############################',
+                            '#####################',
                             '#########################',
-                            '####################',
-                            '#############'
+                            '##################################',
+                            '#######################',
+                            '############',
+                            '#######################',
+                            '######################',
+                            '###########'
                         }:
                     bar.next()
                     continue
-                app_login = str(applst[x][1])
-                client = str(applst[x][0])
-                template = str(applst[x][5])
-                TTH = str(applst[x][6])
-                location = str(joblst[mid][4])
+                app_login = str(app_list[x][1])
+                client = str(app_list[x][0])
+                template = str(app_list[x][5])
+                TTH = str(app_list[x][6])
+                location = str(job_list[mid][4])
 
-                [job1, job2, job3, job4, job5] = str(joblst[mid][7]), \
-                                                 str(joblst[mid][11]), \
-                                                 str(joblst[mid][15]), \
-                                                 str(joblst[mid][19]), \
-                                                 str(joblst[mid][23])
+                [job1, job2, job3, job4, job5] = str(job_list[mid][7]), \
+                                                 str(job_list[mid][11]), \
+                                                 str(job_list[mid][15]), \
+                                                 str(job_list[mid][19]), \
+                                                 str(job_list[mid][23])
 
-                [dep1, dep2, dep3, dep4, dep5] = str(joblst[mid][8]), \
-                                                 str(joblst[mid][12]), \
-                                                 str(joblst[mid][16]), \
-                                                 str(joblst[mid][20]), \
-                                                 str(joblst[mid][24])
+                [dep1, dep2, dep3, dep4, dep5] = str(job_list[mid][8]), \
+                                                 str(job_list[mid][12]), \
+                                                 str(job_list[mid][16]), \
+                                                 str(job_list[mid][20]), \
+                                                 str(job_list[mid][24])
 
                 temp_set = set()
-                temp1 = templates_search(job1, dep1, matrixlst, temp_set)
-                temp2 = templates_search(job2, dep2, matrixlst, temp_set)
-                temp3 = templates_search(job3, dep3, matrixlst, temp_set)
-                temp4 = templates_search(job4, dep4, matrixlst, temp_set)
-                temp5 = templates_search(job5, dep5, matrixlst, temp_set)
+                temp1 = templates_search(job1, dep1, matrix_list, temp_set)
+                temp2 = templates_search(job2, dep2, matrix_list, temp_set)
+                temp3 = templates_search(job3, dep3, matrix_list, temp_set)
+                temp4 = templates_search(job4, dep4, matrix_list, temp_set)
+                temp5 = templates_search(job5, dep5, matrix_list, temp_set)
 
                 result, if_else = set_and_result(temp_set, template)
 
                 writer.writerows(
                     [[
-                        app_login + ";" +  # mms
+                        app_login + ";" +  # app
                         client + ";" +  # CLIENT
                         template + ";" +  # TEMPLATE
                         result + ";" +
                         if_else + ";" +
-                        TTH + ";" +  # SAP
-                        location + ";" +  # STORE
+                        TTH + ";" +  # TTH
+                        location + ";" +  # location
                         job1 + ";" +  # JOB1
                         dep1 + ";" +  # DEP1
                         job2 + ";" +  # JOB2
@@ -166,11 +175,10 @@ if __name__ == '__main__':
                     ]]
                 )
                 bar.next()
-        jobfile.close()
-        appfile.close()
         file.close()
-        matrix.close()
         print("\n\n\t--- %s seconds, yo ---" % round(time.time() - start_time))
-        input('\n\tГотово! Намжите Enter, чтобы выйти.\n')
+        print('\n\tГотово!\n')
     except KeyboardInterrupt:
-        input("\n\n\tВызвано прерывание программы. Нажмите Enter, чтобы выйти.\n")
+        print("\n\n\tВызвано прерывание программы. Завершение работы через 3 секунды.\n")
+        time.sleep(3)
+        sys.exit()
